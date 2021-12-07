7606dcecceb42292934411a71441c54f75da1b01
matthew-sanii <60710943+matthew-sanii@users.noreply.github.com>
2021-12-07 23:21:16 +0000

    edited main.swift

--- main.swift
+++ main.swift
@@ -1,6 +1,22 @@
+public class MrCoxallStacks {
+  private var aStack: Array<Int> = Array()
+
+  func push(pushValue: Int) {
+    print("Adding ", pushValue, " to stack.")
+    aStack.append(pushValue)
+  }
+
+  func showStack() {
+    print("The stacklist.")
+    for value in aStack {
+      print(value)
+    }
+    print("End of stacklist.")
+  }
+}
+
 let stacks = MrCoxallStacks()
   for _ in 1...3 {
-    stacks.push(Int.random(in: 1..<100))
+    stacks.push(pushValue: Int.random(in: 1..<100))
   }
-  stacks.showStack()
-}
+stacks.showStack()
